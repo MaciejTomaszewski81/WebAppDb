5ab2c71a2fd08c9cbcea0b915f9084b24943e4ca
soho.tomaszewski@gmail.com <Debiledwa2>
2022-01-26 15:40:43 +0100

    First Commit

---
+++ src/main/java/pl/tomaszewski/demo/WebAppDbApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszewski.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppDbApplication.class, args);
+    }
+
+}
